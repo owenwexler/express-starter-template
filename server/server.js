4092db210e7e431570b69b823b72de6f8ac24f3c
Owen Wexler <owenbwexler@gmail.com>
2020-10-20 23:46:46 -0400

    Add POST route

--- server/server.js
+++ server/server.js
@@ -6,8 +6,13 @@ const data = [{id: 1, value: 'foo'}, {id, 2: value: 'bar'}];
 
 app.use(express.json());
 
-app.get('/', (req, res) => {
-  res.json(data);
+app.get('/api/data/', (req, res) => {
+  res.status(200).json(data);
+});
+
+app.post('/api/data/', (req, res) => {
+  data.push(req.body);
+  res.status(200).send(req.body);
 });
 
 app.listen(PORT, () => { console.log('Server running on PORT ', PORT) });
