8462dc27d567815f1746a42bded06428851238d0
Owen Wexler <owenbwexler@gmail.com>
2020-11-12 21:22:10 -0500

    Add start script to package.json

--- server/server.js
+++ server/server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const PORT = 3000;
 
-const data = [{id: 1, value: 'foo'}, {id, 2: value: 'bar'}];
+const data = [{id: 1, value: 'foo'}, {id: 2, value: 'bar'}];
 
 app.use(express.json());
 
